26e16d0184d18b935a62ac2c8d95d374fa9395c2
lloyd <tomas.cardell@gmail.com>
2025-04-15 08:46:37 +0200

    Konsole-magi

--- PaperWars.Client/Program.cs
+++ PaperWars.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using PaperWards.DataObjects;
+﻿using Konsole;
+using PaperWards.DataObjects;
 using PaperWards.GameEngine;
 
 //Start a one-player game
@@ -12,8 +13,40 @@ engine.StartGame(game);
 
 //Game loop
 
+var width = 40 * game.Players.Count;
+var gameWindow = Window.OpenBox("Paper Wars");
+var newsWindow = gameWindow.OpenBox("News Cards", 1, 1, width,7, LineThickNess.Double);
+var newsAgencyWindow = gameWindow.OpenBox("NewsAgency Cards", 1, 11, width,7, LineThickNess.Double);
+var reporterWindow = gameWindow.OpenBox("Reporter Pool", 1, 21, width,7, LineThickNess.Double);
+
 foreach (var player in game.Players)
 {
+    showNewsCards();
+
+
+    // showNewsAgencyCards();
+    // showReporterPool();
+    
     if(!player.Passed)
         player.ClaimNews(game);
+}
+
+
+void showNewsCards()
+{
+    var splits = new List<Split>();
+    var i = 1;
+    foreach (var card in game.NewsPool)
+    {
+        splits.Add(new Split(17,"("+ i++.ToString()+ ") " + card.NewsCategory.ToString()));
+    }
+    var cols = newsWindow.SplitColumns(splits.ToArray());
+
+    for (int c = 0; c < cols.Length; c++)
+    {
+        var col = cols[c];
+        col.WriteLine("News value: " + game.NewsPool[c].NewsValue);
+        col.WriteLine("Reporter:   " + game.NewsPool[c].RequiredReporterLevel);
+        col.WriteLine("Reputation: " + game.NewsPool[c].ReputationAjustment);
+    }
 }
